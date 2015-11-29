3e93b73a03bba89001c9ad663c4ccf1dd59362d9
Nitin Chaurasia <gs.nitin@gmail.com>
2015-11-28 23:09:23 -0800

    night of Nov 28

---
+++ src/nitin3/httpServlets/sessionTracking/S1HiddenFormField.java
@@ -0,0 +1,21 @@
+package nitin3.httpServlets.sessionTracking;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class S1HiddenFormField
+ */
+public class S1HiddenFormField extends HttpServlet {
+       
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) 
+			throws ServletException, IOException {
+		
+		
+		
+	}
+
+}
---
+++ src/nitin3/httpServlets/sessionTracking/S5HttpSessionServlet.java
@@ -0,0 +1,21 @@
+package nitin3.httpServlets.sessionTracking;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class S5HttpSessionServlet
+ */
+public class S5HttpSessionServlet extends HttpServlet {
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) 
+			throws ServletException, IOException {
+		
+		request.getAttribute(arg0)
+		
+	}
+
+}
